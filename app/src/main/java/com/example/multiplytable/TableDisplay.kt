a0bfda71a1fc63e76c1eaba6c14d07218afedf6a
Alexajaftha <kirstenjaftha510@gmail.com>
2026-04-01 16:54:46 +0200

    transfered code from main to other and made a bundle to receive it

--- app/src/main/java/com/example/multiplytable/TableDisplay.kt
+++ app/src/main/java/com/example/multiplytable/TableDisplay.kt
@@ -1,6 +1,7 @@
 package com.example.multiplytable
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +12,19 @@ class TableDisplay : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_table_display)
+
+        //get the table number from the bundle
+        //assigning data from main page to table display page
+        val bundle: Bundle? = intent.extras
+        val tableString: String? = bundle?.getString("tableNumber")
+
+        //convert the table number back into an integer
+        val tableNumber = tableString?.toInt()
+
+        //added another variable for the multiply table
+        val multiplyTable = findViewById<TextView>(R.id.tableDisplayText)
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
